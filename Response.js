113354a67006ec862beecdb7ac334ebb8422e92b
Fatunbi David Oluwakayode <fatunbidavidkayode@gmail.com>
2019-09-04 06:57:40 +0200

    Changes to be committed:

---
+++ Response.js
@@ -0,0 +1,9 @@
+
+document.getElementById('btn').onclick = function(){
+    myFunction
+};
+
+function myFunction(){
+    alert('Submission successful. Thank you for reaching out')
+};
+console.log(myFunction);
